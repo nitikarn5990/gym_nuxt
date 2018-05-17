dfef8a7afc4ffd7e2c694b622bb579f1597f3944
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-05-17 15:31:48 +0200

    delete member

--- pages/_memberId/index.vue
+++ pages/_memberId/index.vue
@@ -2,29 +2,28 @@
   <v-layout>
     <v-flex xs12 sm6 offset-sm3>
       <v-card>
-        </v-card-media>
         <v-card-title primary-title>
           <div>
             <h3 class="headline mb-0">Name : {{memberDetail.name}}</h3>
-            <br><br>
-            <div>
-              Mobile : {{memberDetail.mobile}}
-              <br><br>
-              Monthly Subscription : {{memberDetail.monthlySubscription}} LE.
-              <br><br>
-              Subscriped at : {{memberDetail.date}}
-              <br><br>
-              Address : {{memberDetail.address}}
-              <br><br>
-              Member Id : {{memberDetail.memberId}}
-              <br><br>
-              Notes : {{memberDetail.notes}}
+            <br>
+            <div class="information">
+              <strong>Mobile : </strong>{{memberDetail.mobile}}
+              <br>
+              <strong>Monthly Subscription : </strong>{{memberDetail.monthlySubscription}} LE.
+              <br>
+              <strong>Subscriped at : </strong>{{memberDetail.date}}
+              <br>
+              <strong>Address : </strong>{{memberDetail.address}}
+              <br>
+              <strong>Member Id : </strong>{{memberDetail.memberId}}
+              <br>
+              <strong>Notes : </strong>{{memberDetail.notes}}
             </div>
           </div>
         </v-card-title>
         <v-card-actions>
-          <v-btn flat color="orange">Share</v-btn>
-          <v-btn flat color="orange">Explore</v-btn>
+          <v-btn flat color="orange">Edit</v-btn>
+          <v-btn flat color="red" @click="deleteMember()">Delete</v-btn>
         </v-card-actions>
       </v-card>
     </v-flex>
@@ -41,6 +40,26 @@ export default {
     }
   },
   methods: {
+    deleteMember () {
+      if (confirm(`Are you sure that you want to delete ${this.memberDetail.name} ?`) === true) {
+        firebase.database().ref('members').once('value')
+          .then((data) => {
+            const members = []
+            const obj = data.val()
+            for (let key in obj) {
+              if (key == this.$route.params.memberId) {
+                firebase.database().ref('members/' + key).remove()
+                alert(`${this.memberDetail.name} has been deleted successfully!`)
+                this.$router.push({path: '/'})
+              }
+            }
+          })
+          .catch((error) => {
+            alert(error.message)
+          })
+        
+      }
+    }
   },
   created () {
     // firebase.database().ref('members').once('value')
@@ -96,5 +115,7 @@ export default {
 </script>
 
 <style scoped>
-
+.information {
+  white-space: pre-line;
+}
 </style>
